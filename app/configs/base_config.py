ab68d244604ad9bf7f41a7a3020141592ae47dba
Di-Mo-De-OH <dhelahep0115@gmail.com>
2026-02-23 11:37:56 +0900

    install tortoise

---
+++ app/configs/base_config.py
@@ -0,0 +1,17 @@
+from enum import StrEnum
+
+from pydantic_settings import BaseSettings
+
+
+class Env(StrEnum):
+    LOCAL = "local"
+    STAGE = "stage"
+    PROD = "prod"
+
+
+class Config(BaseSettings):
+    MYSQL_HOST: str = "localhost"
+    MYSQL_PORT: int = 3306
+    MYSQL_USER: str = "root"
+    MYSQL_PASSWORD: str = "123123"
+    MYSQL_DB: str = "when2meet_vod"
